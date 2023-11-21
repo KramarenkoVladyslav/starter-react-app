0be9170c70223a08713a20941359bc62ed51c5f2
KramarenkoVladyslav <93004336+KramarenkoVladyslav@users.noreply.github.com>
2023-11-22 00:21:40 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. I'm Vladyslav Kramarenko
+          Edit <code>src/App.js</code> and save to reload. My name is Vladyslav
         </p>
         <a
           className="App-link"
